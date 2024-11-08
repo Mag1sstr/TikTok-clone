afbccc032833f13d5ea555a386d53def4a518514
Mag1sstr <danilkarachev234@mail.ru>
2024-11-08 23:26:33 +0500

    Update Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,13 +1,28 @@
+import { useContext, useState } from "react";
 import "./Search.css";
 import SearchIcon from "@mui/icons-material/Search";
+import { useNavigate } from "react-router-dom";
+import { RequestsContext } from "../../contexts/RequestsContext";
 export default function Search() {
+  const [value, setValue] = useState("");
+  const { searchByKeywords } = useContext(RequestsContext);
+  const navigate = useNavigate();
   return (
-    <form className="search">
+    <form
+      className="search"
+      onSubmit={(e) => {
+        e.preventDefault();
+        navigate(`/search?q=${value}`);
+        searchByKeywords(value);
+      }}
+    >
       <div className="search__row">
         <SearchIcon />
         <input
+          value={value}
           className="search__input"
           type="text"
+          onChange={(e) => setValue(e.target.value)}
           name="search"
           placeholder="Search"
         />
